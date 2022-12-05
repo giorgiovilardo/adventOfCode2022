181ed44631ef93f6a3e0e7ce8d3c762a9506eaf9
Giorgio Vilardo <giorgio@vilardo.eu>
2022-12-05 18:56:05 +0100

    tests infra for d5

---
+++ src/test/scala/solutions/Day5Suite.scala
@@ -0,0 +1,29 @@
+package solutions
+
+import org.scalatest.funsuite.AnyFunSuite
+import utils.Utils
+
+final class Day5Suite extends AnyFunSuite {
+  private val realData = "day5.txt"
+  private val testData = "test_" + realData
+
+  test("Test implementation of part 1 done") {
+//    val file = Utils.readFileAsIterator(testData)
+    assert(true)
+  }
+
+  test("Real implementation of part 1 done") {
+//    val file = Utils.readFileAsIterator(realData)
+    assert(true)
+  }
+
+  test("Test implementation of part 2 done") {
+//    val file = Utils.readFileAsIterator(testData)
+    assert(true)
+  }
+
+  test("Real implementation of part 2 done") {
+//    val file = Utils.readFileAsIterator(realData)
+    assert(true)
+  }
+}
