3b032bbb9c8a56c7323e499fee474ce670c1cd73
Giorgio Vilardo <giorgio@vilardo.eu>
2022-12-05 23:26:37 +0100

    day 5 part 1 done

--- src/main/scala/solutions/Day5.scala
+++ src/main/scala/solutions/Day5.scala
@@ -1,3 +1,77 @@
 package solutions
 
-case object Day5 {}
+import scala.collection.mutable
+
+final case class Move(qty: Int, from: Int, to: Int)
+case object Move {
+  def fromString(s: String): Option[Move] = {
+    val movePattern = raw"move (\d+) from (\d+) to (\d+)".r
+    s match {
+      case movePattern(qty, from, to) =>
+        Some(Move(qty.toInt, from.toInt, to.toInt))
+      case _ => None
+    }
+  }
+}
+
+case object InputParser {
+  def getMoveList(i: List[String]): List[Move] =
+    i.filter(_.startsWith("move")).flatMap(Move.fromString)
+
+  def makeStartingPosition(i: List[String]): List[mutable.Stack[Char]] = {
+    val filteredList =
+      i.filterNot(_.startsWith("move")).filterNot(_ == "")
+
+    val padLength =
+      filteredList.map(_.length).fold(0)((a, v) => if (a > v) a else v)
+
+    filteredList
+      .map(_.padTo(padLength, ' '))
+      .transpose
+      .flatMap(isUselessColumn)
+      .map(_.dropRight(1))
+      .map(_.filterNot(_ == ' '))
+      .map(_.reverse)
+      .map(new mutable.Stack().pushAll(_))
+  }
+
+  private def isUselessColumn(l: List[Char]): Option[List[Char]] = {
+    l match {
+      case x if x.contains('[') => None
+      case x if x.contains(']') => None
+      case x if x.forall(_ == ' ') => None
+      case _ => Some(l)
+    }
+  }
+}
+
+case object Gamestate {
+  type Gamestate = List[mutable.Stack[Char]]
+  def move(g: Gamestate, m: Move): Gamestate = {
+    val Move(times, srcStackNumber, destStackNumber) = m
+    val srcStack = g(srcStackNumber - 1)
+    val destStack = g(destStackNumber - 1)
+    for (_ <- 0 until times) {
+      destStack.push(srcStack.pop())
+    }
+    g
+  }
+
+  def extractSolution(g: Gamestate): String = {
+    g.map(_.pop().toString).fold("")(_ + _)
+  }
+}
+
+case object Day5 {
+  type Gamestate = List[mutable.Stack[Char]]
+  def solvePartOne(i: List[String]): String = {
+    val movelist: List[Move] = InputParser.getMoveList(i)
+    val startingPosition: Gamestate =
+      InputParser.makeStartingPosition(i)
+    Gamestate.extractSolution(
+      movelist.foldLeft(startingPosition)((state: Gamestate, value: Move) =>
+        Gamestate.move(state, value)
+      )
+    )
+  }
+}
--- src/test/scala/solutions/Day5Suite.scala
+++ src/test/scala/solutions/Day5Suite.scala
@@ -8,13 +8,13 @@ final class Day5Suite extends AnyFunSuite {
   private val testData = "test_" + realData
 
   test("Test implementation of part 1 done") {
-//    val file = Utils.readFileAsIterator(testData)
-    assert(true)
+    val file = Utils.readFileAsList(testData)
+    assert(Day5.solvePartOne(file) == "CMZ")
   }
 
   test("Real implementation of part 1 done") {
-//    val file = Utils.readFileAsIterator(realData)
-    assert(true)
+    val file = Utils.readFileAsList(realData)
+    assert(Day5.solvePartOne(file) == "VCTFTJQCG")
   }
 
   test("Test implementation of part 2 done") {
@@ -26,4 +26,49 @@ final class Day5Suite extends AnyFunSuite {
 //    val file = Utils.readFileAsIterator(realData)
     assert(true)
   }
+}
+
+final class MoveSuite extends AnyFunSuite {
+  private val correctString = "move 1 from 2 to 1"
+  private val badString = "move 12 from a to"
+
+  test("Should convert a correct move string") {
+    assert(Move.fromString(correctString).contains(Move(1, 2, 1)))
+  }
+
+  test("Should convert a correct move string with big numbers") {
+    assert(Move.fromString("move 123 from 34 to 5").contains(Move(123, 34, 5)))
+  }
+
+  test("Should not convert a bad move string") {
+    assert(Move.fromString(badString).isEmpty)
+  }
+}
+
+final class InputParserSuite extends AnyFunSuite {
+  private val correctMoveList = List(
+    "move 1 from 2 to 1",
+    "move 121 from 4 to 7",
+    "move 999 from 4 to 55"
+  )
+  private val badMoveList = correctMoveList.appended("move a from 123 from 3")
+
+  test("Should convert a correct move list") {
+    val expected = List(
+      Move(1, 2, 1),
+      Move(121, 4, 7),
+      Move(999, 4, 55)
+    )
+    assert(InputParser.getMoveList(correctMoveList) == expected)
+  }
+
+  test("Should skip bad values") {
+    val startingLength = 4
+    val expectedLength = 3
+    assert(badMoveList.length == startingLength)
+    assert(
+      InputParser.getMoveList(badMoveList).length == expectedLength
+    )
+  }
+
 }
